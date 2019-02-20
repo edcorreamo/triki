432ba206a7b7d3a43bb8079167dc5a3765f4decc
Prometeo <Prometeo@LAPTOP-38R5F0MR>
2019-02-20 17:30:56 -0500

    emmanuel 30-octubre terminada mi parte

---
+++ src/juego/Juego.java
@@ -0,0 +1,11 @@
+
+package juego;
+
+public class Juego extends UIJuego{
+    
+    public static void main(String[] args) {
+        UIJuego ventana=new UIJuego();
+        ventana.setVisible(true);
+    }
+    
+}
